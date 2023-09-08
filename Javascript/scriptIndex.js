c9ae21eac4350331190625a83da4436c574df878
mateus-jaszumbek <mateusjaszumbek@hotmail.com>
2023-09-07 21:39:33 -0300

    commit

--- Javascript/scriptIndex.js
+++ Javascript/scriptIndex.js
@@ -1,22 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const banner = document.querySelector(".cookie-banner");
-    const acceptButton = document.querySelector("#aceitar-cookie");
-    const rejectButton = document.querySelector("#rejeitar-cookie");
+document.addEventListener('DOMContentLoaded', () => {
+    // Seu código JavaScript aqui
 
-    // Verifica se o usuário já fez uma escolha anteriormente
-    if (localStorage.getItem("cookies-aceitos")) {
-        banner.style.display = "none";
-    }
+    // Função para definir um cookie com a preferência do usuário
+function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
 
-    // Ação ao clicar em "Aceitar"
-    acceptButton.addEventListener("click", function () {
-        localStorage.setItem("cookies-aceitos", "true");
-        banner.style.display = "none";
-    });
+// Função para ocultar o banner de consentimento
+function hideCookieBanner() {
+    const cookieBanner = document.getElementById('cookie-consent');
+    cookieBanner.style.display = 'none';
+}
 
-    // Ação ao clicar em "Rejeitar"
-    rejectButton.addEventListener("click", function () {
-        localStorage.setItem("cookies-aceitos", "false");
-        banner.style.display = "none";
-    });
+// Event listener para aceitar cookies
+document.getElementById('accept-cookies').addEventListener('click', () => {
+    setCookie('cookies-accepted', 'true', 365); // Armazena a preferência do usuário por 365 dias
+    hideCookieBanner();
 });
+
+// Event listener para rejeitar cookies
+document.getElementById('reject-cookies').addEventListener('click', () => {
+    setCookie('cookies-accepted', 'false', 365);
+    hideCookieBanner();
+});
+
+// Verifica se o usuário já fez uma escolha anteriormente
+window.addEventListener('load', () => {
+    const cookiesAccepted = document.cookie.includes('cookies-accepted=true');
+    if (!cookiesAccepted) {
+        const cookieBanner = document.getElementById('cookie-consent');
+        cookieBanner.style.display = 'block';
+    }
+});
+
+});
+
+
